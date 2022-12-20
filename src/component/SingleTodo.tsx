ee12226af3affcaed73cfaf9453d7622fc17f360
rushikesh-scalex <rushikeshm@scalextechnologies.com>
2022-12-20 15:43:50 +0530

    icons reversed

--- src/component/SingleTodo.tsx
+++ src/component/SingleTodo.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { deleteTodoFromList, editTodoFromList } from "../redux/Actions";
-import "../Styles/SingleTodo.css"
+import "../Styles/SingleTodo.css";
 interface Props {
   id: number;
   data: string;
@@ -19,42 +19,46 @@ const SingleTodo: React.FC<Props> = ({ id, data }) => {
     setEdit(false);
   };
   return (
-    <form className="singleTodo" onSubmit={(e) => handleSubmit(e, id, editTodo)}>
-     <div className="mainData">
-     {edit ? (
-        <>
-          <input className="editTodoInput"
-            autoFocus={true}
-            type="text"
-            value={editTodo}
-            onChange={(e) => setEditTodo(e.target.value)}
-          />
-        </>
-      ) : (
-        <>
-          <div>
-            <span>{data} </span>
-          </div>
-        </>
-      )}
-     </div>
+    <form
+      className="singleTodo"
+      onSubmit={(e) => handleSubmit(e, id, editTodo)}
+    >
+      <div className="mainData">
+        {edit ? (
+          <>
+            <input
+              className="editTodoInput"
+              autoFocus={true}
+              type="text"
+              value={editTodo}
+              onChange={(e) => setEditTodo(e.target.value)}
+            />
+          </>
+        ) : (
+          <>
+            <div>
+              <span>{data} </span>
+            </div>
+          </>
+        )}
+      </div>
       <div>
-      <span>
-        <i
-          title="Delete"
-          onClick={() => dispatch(deleteTodoFromList(id))}
-          className="fa fa-trash span_icons"
-        ></i>
-      </span>
-      <span>
-        <i
-          title="Edit"
-          onClick={() => {
-            setEdit(true);
-          }}
-          className="fa-solid fa-pen-to-square span_icons"
-        ></i>
-      </span>
+        <span>
+          <i
+            title="Edit"
+            onClick={() => {
+              setEdit(true);
+            }}
+            className="fa-solid fa-pen-to-square span_icons"
+          ></i>
+        </span>
+        <span>
+          <i
+            title="Delete"
+            onClick={() => dispatch(deleteTodoFromList(id))}
+            className="fa fa-trash span_icons"
+          ></i>
+        </span>
       </div>
     </form>
   );
